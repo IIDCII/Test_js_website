93f27ae34f78bef4b37e6c98b2e1fd4399c6cfd8
Dario Cline <darioclinemp3@gmail.com>
2023-04-02 19:39:08 +0100

    got some of the code to start working

--- test-app/README.md
+++ test-app/README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+On 12:40
+https://www.youtube.com/watch?v=hQAHSlTtcmY&ab_channel=WebDevSimplified
--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,8 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// useState is kind of like getting the context for the application
+import React, {useState} from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return null
+
+  // object destructuring -> extract data from arrays and store them to variables
+  const [todos, setTodos] = useState(['Todo 1','Todo 2'])
+  
+  
+  // remember that you can only return one thing at a time
+  // so if you want to add more than one then make a fragment as shown
+  return (
+    <>
+    <TodoList todos = {todos}/>
+    <input type="text"/>
+    <button>Add Todo</button>
+    <button>Clear Completed Todos</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
